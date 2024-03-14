984b57f039faad3510c9625363dcdd7a4f85c7db
nooneeyam <anishshahi344@gmail.com>
2024-03-14 16:29:14 +0545

    js operators

---
+++ app.js
@@ -0,0 +1,40 @@
+
+//operators
+let a = 5;
+let b = 2;
+console.log("a =", a , "& b = ", b)
+console.log(a + b);
+//uniary operators (increment & decreament)
+++a , --a //pre incre/decre
+a++ , a-- //post 
+
+console.log(a);
+a += 10; //assignment opertors
+b -= 5;
+a *= 5;
+a /= 5;
+a **= 5;
+console.log(a)
+console.log(a === b);//comparision operators
+console.log(a !== b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+
+
+//uniary operators (increment & decreament)
+++a , --a //pre incre/decre
+a++ , a-- //post 
+
+//logical operators
+let anish = 27
+let shahi = 10
+let condition1 = anish === shahi;
+let condition2 = anish > shahi
+console.log(condition1 && condition2)// and oper
+console.log(condition1 || condition2)// or operat
+//console.log(condition1 ! condition2)// not opert
+
+
+
